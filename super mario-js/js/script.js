91ce20d8aa0ff9ab5f8646c75a3f2f3a472b4f86
GIBI <guilherme64station.@gmail.com>
2024-10-07 11:37:42 -0300

    super-mario-commit

---
+++ super mario-js/js/script.js
@@ -0,0 +1,34 @@
+    const mario = document.querySelector('.mario');
+    const pipe = document.querySelector('.pipe')
+    const jump = () => {
+        mario.classList.add('jump');
+
+        setTimeout(() => {
+            mario.classList.remove('jump');
+        }, 500);
+    }
+
+ const loop = setInterval(() => {
+
+    const pipeposition = pipe.offsetLeft;
+    const marioposition = +window.getComputedStyle(mario).bottom.replace('px','');
+
+    if (pipeposition < 120 &&pipeposition  > 0 && marioposition < 80) {
+
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipeposition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioposition}px`;
+
+        mario.src = './img/game-over.png';
+        mario.style.width = '75px'
+        mario.style.marginleft = '50px'
+
+        clearInterval(loop);
+        
+    }
+
+ }, 10);
+
+document.addEventListener('keydown', jump);
